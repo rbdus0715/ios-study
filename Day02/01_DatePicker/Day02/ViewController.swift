59ae68806c548d896951f8dc59cab950d2a590ab
rbdus0715 <rbdus0715@gmail.com>
2025-09-02 16:50:34 +0900

    Study: day2 - DatePicker

---
+++ Day02/01_DatePicker/Day02/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Day02
+//
+//  Created by gyuyeon jo on 9/2/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let timeSelector: Selector = #selector(ViewController.updateTime)
+    let interval = 1.0
+    var count = 0
+    
+    @IBOutlet var labelCurrentTime: UILabel!
+    @IBOutlet var labelPickerTime: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        Timer.scheduledTimer(timeInterval: interval, target: self, selector: timeSelector, userInfo: nil, repeats: true)
+    }
+
+    @IBAction func changeDatePicker(_ sender: UIDatePicker) {
+        let datePickerView = sender
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm EEE"
+        labelPickerTime.text = "선택시간: " + formatter.string(from: datePickerView.date)
+    }
+    
+    @objc func updateTime() {
+        labelCurrentTime.text = String(count)
+        count = count + 1
+        
+        let date = NSDate()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss EEE"
+        labelCurrentTime.text = "현재시간: " + formatter.string(from: date as Date)
+    }
+}
+
---
+++ Day02/01_DatePicker/Day02Tests/Day02Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Day02Tests.swift
+//  Day02Tests
+//
+//  Created by gyuyeon jo on 9/2/25.
+//
+
+import Testing
+@testable import Day02
+
+struct Day02Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
