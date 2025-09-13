856c7ef654f8f2449424b294d8988457c6b9aed4
rbdus0715 <rbdus0715@gmail.com>
2025-09-13 17:44:29 +0900

    Init: Day04 - Navigation Controller

---
+++ Day04/NavigationController/NavigationController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NavigationController
+//
+//  Created by gyuyeon jo on 9/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Day04/NavigationController/NavigationControllerTests/NavigationControllerTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationControllerTests.swift
+//  NavigationControllerTests
+//
+//  Created by gyuyeon jo on 9/13/25.
+//
+
+import Testing
+@testable import NavigationController
+
+struct NavigationControllerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
